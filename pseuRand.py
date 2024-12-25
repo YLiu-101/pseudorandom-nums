365de4e914f7c1d2c485b6a95a6fdf665c64d91e
Yliu-101 <kailiu989@gmail.com>
2024-12-25 10:05:45 -0600

    plotting out nonuniform distributions

--- pseuRand.py
+++ pseuRand.py
@@ -2,6 +2,9 @@ import time
 from lfsr import main, next_lfsr
 import numpy as np
 import math
+import random
+import matplotlib.pyplot as plt
+random.seed(5000)
 class pseuRand:
     def __init__(self):
         self.bi = (1 << 127) | 1
@@ -23,18 +26,27 @@ class pseuRand:
         """
         seconds = time.time()
         bin_length = math.floor(math.log(sides)/math.log(2))+1
-        print(bin_length, ((1 << bin_length)-1))
-        for i in range(int(round(seconds))**2 % 1000):
+        bin_num = 0
+        a = []
+        # print(bin_length, "{:08bf}".format(((1 << bin_length)-1)))
+        x = random.randint(500,1000)
+        for i in range(x):
             self.bi = next_lfsr(self.bi)
-        while (self.bi & ((1 << bin_length)-1)) > sides-1:
-            # print((self.bi & ((1 >> bin_length)-1)))
+            bin_num = (((bin_num << 1) | (self.bi & 1)) & ((1 << bin_length)-1))
+            a.append(bin_num)
+            # print(bin_num)
+        while (bin_num & ((1 << bin_length)-1)) > sides-1:
             self.bi = next_lfsr(self.bi)
-        return (self.bi & ((1 << bin_length)-1)) + 1
+            bin_num = (bin_num << 1) | (self.bi & 1)
+        a = np.array(a)
+        plt.hist(a, bins=np.arange(a.min(), a.max()+1))
+        plt.show()
+        return (bin_num & ((1 << bin_length)-1)) + 1
 if __name__ == "__main__":
     rando = pseuRand()
     print(rando.coinFlip())
     a = []
-    n = 5000
+    n = 500
     # Coin Flipping 
     # for i in range(n):
     #     a.append(rando.coinFlip())
