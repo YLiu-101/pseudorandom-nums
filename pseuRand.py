06477160019723cce8ca7a8ad56903e246719206
Yliu-101 <kailiu989@gmail.com>
2024-12-25 10:46:42 -0600

    fixed (?) the error, still need to figure out what caused it

--- pseuRand.py
+++ pseuRand.py
@@ -16,7 +16,7 @@ class pseuRand:
     def coinFlip(self): #randomly ouputs a 0 or a 1
         seconds = time.time()
         
-        for i in range(int(round(seconds))**2 % 1000):
+        for i in range(int(round(seconds))**2 % 100):
             self.bi = next_lfsr(self.bi)
         self.num = self.bi & 1
         return self.num
@@ -25,28 +25,26 @@ class pseuRand:
         Models an n-sided die
         """
         seconds = time.time()
-        bin_length = math.floor(math.log(sides)/math.log(2))+1
-        bin_num = 0
+        bin_length = int(math.floor(math.log2(sides))+1)
+        bin_num = sides+1
         a = []
         # print(bin_length, "{:08bf}".format(((1 << bin_length)-1)))
-        x = random.randint(500,1000)
-        for i in range(x):
-            self.bi = next_lfsr(self.bi)
-            bin_num = (((bin_num << 1) | (self.bi & 1)) & ((1 << bin_length)-1))
-            a.append(bin_num)
-            # print(bin_num)
-        while (bin_num & ((1 << bin_length)-1)) > sides-1:
-            self.bi = next_lfsr(self.bi)
-            bin_num = (bin_num << 1) | (self.bi & 1)
-        a = np.array(a)
-        plt.hist(a, bins=np.arange(a.min(), a.max()+1))
-        plt.show()
-        return (bin_num & ((1 << bin_length)-1)) + 1
+        # x = random.randint(500,1000)
+        while bin_num > sides-1:    
+            bin_num = 0
+            for i in range(sides):
+                bin_num = (((bin_num << 1) | (self.coinFlip())) & ((1 << bin_length)-1))
+                # a.append(bin_num)
+                # print(bin_num)
+        # a = np.array(a)
+        # plt.hist(a, bins=np.arange(a.min(), a.max()+1))
+        # plt.show()
+        return bin_num + 1
 if __name__ == "__main__":
     rando = pseuRand()
     print(rando.coinFlip())
     a = []
-    n = 500
+    n = 5000
     # Coin Flipping 
     # for i in range(n):
     #     a.append(rando.coinFlip())
@@ -57,7 +55,7 @@ if __name__ == "__main__":
 
     # Die Rolling
     for i in range(n):
-        a.append(rando.nSideDie(3))
+        a.append(rando.nSideDie(7))
     unique_elements, counts = np.unique(a, return_counts=True)
     print(unique_elements)
     print(counts,np.array(counts)/n)
