e993a6eecdb4bc782508e898909936bd2b1e1b36
Yliu-101 <kailiu989@gmail.com>
2024-12-25 09:22:50 -0600

    created a coinflipper using the pseudrandom number geneator

--- lfsr.py
+++ lfsr.py
@@ -1,14 +1,20 @@
-state = (1 << 127) | 1
-orig_state = state
-counter = 0
-length = len(bin(state)[2:])
-while(True):
-    # print("{:08b}".format(state),f"size {len(bin(state)[2:])}")
-    print(state & 1,end='')
+def next_lfsr(state):
     newbit = (state ^ (state >> 1) ^ (state >> 2) ^ (state >> 7)) & 1
-    state = (state >> 1) | (newbit << (127))
-    counter+=1
-    if (state == orig_state):
-        print(f"Ended after {counter} iterations with size {length} binary number")
-        break
+    return (state >> 1) | (newbit << 127)
 
+def main():
+    state = (1 << 127) | 1
+    orig_state = state
+    counter = 0
+    length = len(bin(state)[2:])
+    while(True):
+        # print("{:08b}".format(state),f"size {len(bin(state)[2:])}")
+        print(state & 1,end='')
+        state = next_lfsr(state)
+        counter+=1
+        if (state == orig_state):
+            print(f"Ended after {counter} iterations with size {length} binary number")
+            break
+
+if __name__ == "__main__":
+    main()
---
+++ pseuRand.py
@@ -0,0 +1,31 @@
+import time
+from lfsr import main, next_lfsr
+import numpy as np
+class pseuRand:
+    def __init__(self):
+        self.bi = (1 << 127) | 1
+        for i in range(int(round(time.time()))**2 % 10000):
+            self.bi = next_lfsr(self.bi)
+        self.num = 0
+    def printNums(self):
+        main()
+    def coinFlip(self): #randomly ouputs a 0 or a 1
+        seconds = time.time()
+        
+        for i in range(int(round(seconds))**2 % 1000):
+            # print("{:08b}".format(self.bi))
+            self.bi = next_lfsr(self.bi)
+        self.num = self.bi & 1
+        return self.num
+
+if __name__ == "__main__":
+    rando = pseuRand()
+    print(rando.coinFlip())
+    a = []
+    n = 50000
+    for i in range(n):
+        a.append(rando.coinFlip())
+    unique_elements, counts = np.unique(a, return_counts=True)
+    print(unique_elements)
+    print(counts,np.array(counts)/n)
+    # print(a)
